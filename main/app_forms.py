9a80df287e207e48b80917f8cfa72966c5b951e1
Robert Musili <robersi366@gmail.com>
2023-11-24 16:05:15 +0300

    Updated app_forms

---
+++ main/app_forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Expense, Category
+
+
+class ExpenseForm(forms.ModelForm):
+    class Meta:
+        model = Expense
+        fields = ['category', 'amount', 'description', 'date']
+
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name']
